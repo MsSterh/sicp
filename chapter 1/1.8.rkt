1309aae9f9236670addea2711b71fb25d514bb46
Anastasiia Bakuta <ae@anahoret.com>
2017-06-30 11:13:00 +0300

    Create 1.8 for cube root

--- chapter 1/1.8.rkt
+++ chapter 1/1.8.rkt
@@ -1,38 +1,32 @@
 #lang racket
 
-;(define (average x y)
-;  (/ (+ x y) 2))
-
 (define (square x)
   (* x x))
 
-(define (average x y)
-  (/ (+ (/ x (square y)) (* 2 y)) 3))
+(define (cube x)
+  (* x x x))
 
 (define (improve guess x)
-  (average guess (/ x guess)))
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
 
 (define (good-enough? guess x)
-  (< (abs (- 1 (/ (square guess) x))) 0.0001))
+  (< (abs (- 1 (/ (cube guess) x))) 0.00001))
 
-(define (sqrt-iter guess x)
+(define (cube-iter guess x)
   (if (good-enough? guess x)
       guess
-      (sqrt-iter (improve guess x)
+      (cube-iter (improve guess x)
                  x)))
-(define (sqrt x)
-  (sqrt-iter 1.0 x))
 
-(sqrt 10000)
-(sqrt 0.0004)
-(sqrt 9000000)
+(define (cube-root x)
+  (cube-iter 1.0 x))
 
 (module+ test
   (require rackunit)
 
   (test-case
-   "sqrt(x) by Newton method"
+   "cube-root(x) by Newton method"
 
-   (check-= (sqrt 10000) 100.0 0.0001 "I work")
-   (check-= (sqrt 0.0004) 0.02 0.0000001 "I work")
-   (check-= (sqrt 9000000) 3000 0.11 "I work")))
+   (check-= (cube-root 27000000) 300.0 0.0001 "I work")
+   (check-= (cube-root 0.000008) 0.02 0.0000001 "I work")
+   (check-= (cube-root 3375) 15 0.0001 "I work")))
